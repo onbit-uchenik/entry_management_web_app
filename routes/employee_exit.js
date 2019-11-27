c1c16f0b1110e4e32dc13ca65900e54958b7dfd3
Anubhav-Vats <onbitsyn@gmail.com>
2019-11-27 21:08:08 +0530

    employee/exit/ end point Tested=[OK]

--- routes/employee_exit.js
+++ routes/employee_exit.js
@@ -10,26 +10,44 @@ employee_exit.use(bodyParser.urlencoded({extended:true}));
 
 employee_exit.route('/')
 
-.get(function(req,res,next){
-//   res.setHeader('Content-Type','text/html');
+.get(function(req,res,next){  
    let path_to_directory =  path.dirname(__dirname);
    res.sendFile(path_to_directory + '/public/employee/exit.html');
 })
 .post(function(req,res,next){
       let date = new Date();
-      let values = [req.body.phone_no,date.getCompleteDate(),req.body.exit_gate];
+      let timestamp = date.getCompleteDate();
+      
       (async function(){
          try{
-            let result = await db.exit(values);
-            res.statusCode = 200;
-            res.json(result);
+            let values = [req.body.emp_id];
+            let result = await db.getEmp(values);
+            if(result.status === 1) {
+               // successful in getting emp details
+               // exit nicely....
+               await make_exit(req,res,result.phone_no,timestamp);
+            }
+            else if(result.status === 4){
+               //employee with emp id do not exist...
+               console.log("employee does not exist...")
+               res.statusCode = 404;
+               res.json(result);
+               res.end();
+            }
+            else{
+               // error while getting emp details..
+               res.statusCode = 500;;
+               res.join(result);
+               res.end();
+            }
          }
-         catch(err) {
+         catch(err){
             res.statusCode = 500;
             res.json({"status":0});
-            console.log(__filename + err);                         
-         }
-         res.end();
+            console.log(__filename + err);
+            res.end();
+         }         
+      
       })();
 
 })
@@ -44,5 +62,20 @@ employee_exit.route('/')
   res.end();
 });
 
+async function make_exit(req,res,phone_no,timestamp){
+   try{
+      let values=[phone_no,timestamp,req.body.exit_gate];
+      let result = await db.exit(values);
+      res.statusCode = 200;
+      res.json(result);
+   }
+   catch(err) {
+      res.statusCode = 500;
+      res.json({"status":0});
+      console.log(__filename + err);                         
+   }
+   res.end();
+
+}
 
 module.exports = employee_exit;
